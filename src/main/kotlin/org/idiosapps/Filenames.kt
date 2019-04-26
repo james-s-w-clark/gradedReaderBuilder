fc3af7068de416b1f6c4bf6a120113e682acb95e
James <IdiosApps@users.noreply.github.com>
2019-04-27 00:37:57 +0100

    Use a top-level directory for input files Makes built distributions simple to work with whilst avoiding clutter (could .gitignore the files in top-most directory and make UX better... Will see if it gets requested!)

--- src/main/kotlin/org/idiosapps/Filenames.kt
+++ src/main/kotlin/org/idiosapps/Filenames.kt
@@ -1,16 +1,16 @@
 package org.idiosapps
 
 class Filenames {
-    val resourcePrefix: String = "./src/main/resources/org.idiosapps/"
+    val inputPrefix: String = "./input/"
     val outputPrefix: String = "./output/"
 
 
-    val inputHeaderFilename: String = resourcePrefix + "header"
-    val inputTitleFilename: String = resourcePrefix + "title"
-    val inputStoryFilename: String = resourcePrefix + "story"
+    val inputHeaderFilename: String = inputPrefix + "header"
+    val inputTitleFilename: String = inputPrefix + "title"
+    val inputStoryFilename: String = inputPrefix + "story"
 
-    val inputVocabFilename: String = resourcePrefix + "vocab"
-    val inputKeyNamesFilename: String = resourcePrefix + "names"
+    val inputVocabFilename: String = inputPrefix + "vocab"
+    val inputKeyNamesFilename: String = inputPrefix + "names"
 
     val outputStoryFilename: String = outputPrefix + "outputStory.tex"
     val outputPDFFilename: String = outputPrefix + "outputStory.pdf"
